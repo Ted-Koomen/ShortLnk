3c0b6a60fc9fb8d2b234a6ab726d9e06ae86c573
Ted-Koomen <koomen.ted@gmail.com>
2017-08-03 21:24:22 -0400

    add login component and route'

--- client/main.js
+++ client/main.js
@@ -5,14 +5,18 @@ import { Router, Route, browserHistory } from 'react-router';
 
 import Signup from '../imports/ui/Signup';
 import Link from '../imports/ui/Link';
+import NotFound from '../imports/ui/NotFound';
+import Login from '../imports/ui/Login';
 
 const routes = (
   <Router history={browserHistory}>
+    <Route path="/" component={Login} />
     <Route path="/signup" component={Signup} />
     <Route path="/links" component={Link} />
+    <Route path="*" component={NotFound} />
   </Router>
-);
+)
 
 Meteor.startup(() => {
-  ReactDOM.render(routes, document.getElementById('app'))
-})
+  ReactDOM.render(routes, document.getElementById('app'));
+});
